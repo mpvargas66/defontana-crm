cdc559c3c48afefa108ec4c5266ee1e01d92a5b3
Marco <marco.vargas.puebla@gmail.com>
2026-08-15 10:12:08 -0400

    feat: switch to @vercel/postgres

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,8 +1,8 @@
-import postgres from 'postgres';
+import { sql } from '@vercel/postgres';
 
 if (!process.env.DATABASE_URL) {
   throw new Error('DATABASE_URL is not set');
 }
 
-export const sql = postgres(process.env.DATABASE_URL);
+export { sql };
 export const db = sql;
